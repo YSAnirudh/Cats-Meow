9d7b032b4e88e8271664f239ff2c0950b6411ca5
YS Anirudh <ysanirudh@gmail.com>
2023-04-27 15:32:25 -0700

    Added UE5 project

---
+++ Source/CatsMeow/CatsMeow.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CatsMeow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CatsMeow, "CatsMeow" );
